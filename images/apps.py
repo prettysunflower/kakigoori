1ecb4551e48413af9b7dc4fb5ee18acf502177ef
Remilia Da Costa Faro <remilia@remilia.ch>
2025-05-04 16:02:00 +0200

    Delete object from S3 when delete image variant

--- images/apps.py
+++ images/apps.py
@@ -1,4 +1,13 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from images.utils import get_b2_resource
+
+
+@receiver(post_delete)
+def delete_image_from_s3_if_variant_is_deleted(sender, instance, **kwargs):
+    bucket = get_b2_resource()
+    bucket.delete_objects(Delete={"Objects": [{"Key": instance.backblaze_filepath}]})
 
 
 class ImagesConfig(AppConfig):
